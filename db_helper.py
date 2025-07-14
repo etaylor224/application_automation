e4ad1859a6bb6237e56a9417eb300c38291db4c4
etaylor224 <etay279@wgu.edu>
2025-07-14 12:41:42 -0500

    Update db_helper.py
    rewrote majority of functions utilizing psycopg

--- db_helper.py
+++ db_helper.py
@@ -1,156 +1,161 @@
-import asyncpg
-import asyncio
+import yaml
+import psycopg2 as psy
+from psycopg2.extras import execute_batch
+
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+_pg_url = config['database_url_test']
 
 
-_pg_url = "postgresql://postgres@localhost/test"
 #TODO Fix url
 #TODO add error handling for all pgsql funcs
 
+def insert_into_search(position, location):
+    with psy.connect(_pg_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """INSERT INTO searchquery(query,location) 
+    VALUES(%s, %s) RETURNING id""", (position, location))
+            return cur.fetchone()[0]
 
-def check_tables(conn, table):
-    query = f"""SELECT EXISTS ( 
-    SELECT 1 
-    FROM information_schema.tables
-    WHERE table_schema = 'public'
-    AND table_name = '{table}'
-"""
-    search = conn.fetch(query)
-    if search == False:
-        return False
-    else:
-        return True
+def insert_data(table: str, data: list):
+    with psy.connect(_pg_url) as conn:
+        with conn.cursor() as cur:
+            try:
+                for entry in data:
+                    already_exists = check_for_duplicates(cur, table, entry)
+                    if already_exists == False:
+                        print("not in db")
+                        execute_batch(cur,
+                                      f"""
+                            INSERT INTO {table}(
+                            job_title,
+                            employer_name,
+                            employer_web,
+                            employment_type,
+                            publisher,
+                            apply_link,
+                            job_description,
+                            remote,
+                            score)          
 
-async def create_tables(conn):
+                            VALUES( 
+                            %(job_title)s,
+                            %(employer_name)s, 
+                            %(employer_web)s,
+                            %(employment_type)s,
+                            %(publisher)s,
+                            %(apply_link)s,
+                            %(job_description)s,
+                            %(remote)s,
+                            %(score)s
+                            )
+                            """,
+                                      data)
+                        conn.commit()
+                    else:
+                        print("Entry exists")
+            except Exception as e:
+                print("Error")
+                print(e)
 
-    table_and_query = {
-    "searchquery_table" : """
-        CREATE TABLE searchquery(
-            id interger PRIMARY KEY,
-            query character varying(50),
-            location character varying(6),
-        )
-    """,
-    "low_result_table" : """
-    CREATE TABLE low_results(
-    id integer PRIMARY KEY, 
-    job_title character varying(100),
-    employer_name character varying (512),
-    employer_web character varying (512),
-    employment_type character varying (100),
-    publisher character varying (150),
-    apply_link character varying(1024),
-    job_description text,
-    remote boolean,
-    score real)
-    """,
-    "high_result_table" : """
-    CREATE TABLE high_results(
-    id integer PRIMARY KEY, 
-    job_title character varying(100),
-    employer_name character varying (512),
-    employer_web character varying (512),
-    employment_type character varying (100),
-    publisher character varying (150),
-    apply_link character varying(1024),
-    job_description text,
-    remote boolean,
-    score real)
-    """,
-    "applied_table" : """
-    CREATE TABLE applied(
-    id integer PRIMARY KEY, 
-    job_title character varying(100),
-    employer_name character varying (512),
-    employer_web character varying (512),
-    employment_type character varying (100),
-    publisher character varying (150),
-    apply_link character varying(1024),
-    job_description text,
-    remote boolean,
-    score real)
-    """
-    }
+def insert_into_applied(data: list):
+    with psy.connect(_pg_url) as conn:
+        with conn.cursor() as cur:
+            try:
+                for entry in data:
+                    already_exists = check_for_duplicates(cur, "applied", entry)
+                    if already_exists == False:
+                        print("not in db")
+                        execute_batch(cur,
+                                      """
+                            INSERT INTO applied(
+                            job_title,
+                            employer_name,
+                            employer_web,
+                            employment_type,
+                            publisher,
+                            apply_link,
+                            job_description,
+                            remote,
+                            score)          
 
-    for entry in table_and_query:
-        check = check_tables(conn, entry)
-        if not check:
-            query = f"""CREATE TABLE {entry}(
-                    {table_and_query[entry]})"""
-            await conn.execute(query)
+                            VALUES( 
+                            %(job_title)s,
+                            %(employer_name)s, 
+                            %(employer_web)s,
+                            %(employment_type)s,
+                            %(publisher)s,
+                            %(apply_link)s,
+                            %(job_description)s,
+                            %(remote)s,
+                            %(score)s
+                            )
+                            """,
+                                      data)
+                        conn.commit()
+                    else:
+                        print("Entry exists")
+            except Exception as e:
+                print("Error")
+                print(e)
 
-
-async def insert_into_search(conn, position, location):
-
-    query = f"""
-    INSERT INTO searchquery_table(query,location) 
-    VALUES($1, $2)
-    """, position, location
-    await conn.execute(query)
-
-async def insert_into_low(conn, data: list):
-    #9 columns for each table
-
-    query = """
-    INSERT INTO low_result_table(job_title,
-    employer_name,
-    employer_web,
-    employment_type,
-    publisher,
-    apply_link,
-    job_description,
-    remote,
-    score) 
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
-    """, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]
-
-    await conn.execute(query)
-
-
-async def insert_into_high(conn, data: list):
-    query = """INSERT INTO high_result_table(job_title,
-    employer_name,
-    employer_web,
-    employment_type,
-    publisher,
-    apply_link,
-    job_description,
-    remote,
-    score) 
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) 
-    """, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]
-
-    await conn.execute(query)
-
-async def insert_into_applied(conn, data: list):
-    query = """INSERT INTO applied_table(job_title,
-    employer_name,
-    employer_web,
-    employment_type,
-    publisher,
-    apply_link,
-    job_description,
-    remote,
-    score) 
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) 
-    """, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]
-
-    await conn.execute(query)
 
 def remove_row():
     #TODO this will be a call to remove a tow
     pass
 
-async def populate_table_call(table):
-    #TODO query table data to populate web page, any table
-    query = f"""
-    SELECT * FROM {table}
-    """
 
-async def check_for_duplicate():
-    pass
+def populate_table_call(table):
+    """Retrieves data from SQL DB and returns a dict for front end parsing"""
+    with psy.connect(_pg_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM public.{table}")
+            data = cur.fetchall()
+            return data
 
 
-async def create_pg_conn():
-    """Opens connection to posgresql database and returns connection"""
-    return await asyncpg.connect(_pg_url)
+def check_for_duplicates(conn, table: str, data: dict):
+    conn.execute(f"""
+        SELECT EXISTS(SELECT 1 FROM {table} WHERE
+        job_title = %s AND
+        employer_name = %s AND
+        employer_web = %s AND
+        employment_type = %s AND
+        publisher = %s AND
+        apply_link = %s AND
+        job_description = %s)    
+        """, (
+        data['job_title'], data['employer_name'], data['employer_web'], data["employment_type"], data["publisher"],
+        data["apply_link"], data["job_description"]))
+    check = conn.fetchone()
 
+    if check[0]:
+        return True
+    else:
+        return False
+
+
+def job_data_helper(data):
+    column_names = [
+        "id",
+        "job_title",
+        "employer_name",
+        "employer_web",
+        "employment_type",
+        "publisher",
+        "apply_link",
+        "job_description",
+        "remote",
+        "score",
+    ]
+
+    cleaned = list()
+    for entry in data:
+        rows = entry
+        data_dict = dict()
+        for i in range(len(column_names)):
+            data_dict[column_names[i]] = rows[i]
+        cleaned.append(data_dict)
+    return cleaned
